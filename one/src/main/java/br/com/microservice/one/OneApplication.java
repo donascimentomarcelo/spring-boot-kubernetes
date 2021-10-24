b1251a2c936ece0839e0fe194253c3f4ae80f23b
Marcelo <marcelojunin2010@hotmail.com>
2021-10-24 20:14:50 -0300

    setting up eureka client

--- one/src/main/java/br/com/microservice/one/OneApplication.java
+++ one/src/main/java/br/com/microservice/one/OneApplication.java
@@ -2,10 +2,10 @@ package br.com.microservice.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "app-two")
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class OneApplication {
